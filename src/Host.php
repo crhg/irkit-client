1913566faf5a0fb26d1c6dc787a971a4f0a5f8a8
Manabu Matsui <manabu.matsui@gmail.com>
2017-09-23 11:07:20 +0900

    delete unused code

--- src/Host.php
+++ src/Host.php
@@ -19,13 +19,6 @@ class Host
 
     public function __construct($name, Config $config)
     {
-        $host_config = $config->getOrFail("host.$name");
-        ['host'][$name];
-
-        if (!isset($host_config['uri'])) {
-            throw new \Exception('no uri: host='.$name);
-        }
-
         $this->uri = $config->getOrFail("host.$name.uri");
         $this->http_option = $config->get("host.$name.http_option", []);
         $this->retry = $config->get("host.$name.retry", self::DEFAULT_RETRY);
